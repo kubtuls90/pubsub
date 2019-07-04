c65027aa5da922fafa83c6d3ae6eaf233ea08bad
kubtuls90 <52516690+kubtuls90@users.noreply.github.com>
2019-07-03 19:29:19 -0700

    Update EventBus.java

--- ProjectPubSub/src/eventbus/EventBus.java
+++ ProjectPubSub/src/eventbus/EventBus.java
@@ -1,1 +1,22 @@
+/**
+ * 
+ */
+package eventbus;
 
+/**
+ * @author Tkuber
+ *
+ */
+public interface EventBus {
+
+    // Object
+    void publishEvent(Object o);
+
+    // Denote Subscriber
+    void addSubscriber(Subscriber jazz);
+
+    //To allow or not to allow clients the ability to filter events received?
+    //void addSubscriberForFilteredEvents(Event event, Subscriber sub);
+    
+    void removeSubscriber(Subscriber sub);
+}
